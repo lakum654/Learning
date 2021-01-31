ea01ec3f78b9174caa3ba53bfffb3fbc0e163674
Lakum Mahendra <lakummahendra654@gmail.com>
2021-01-30 23:47:20 -0800

    Modify at Room

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,94 +1,98 @@
-@extends('layouts.app')
-
-
-@section('content')
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-
-<meta name="csrf-token" content="{{ csrf_token() }}" />
-
-
-<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">Posts</div>
-
-
-                <div class="card-body">
-                    @if($posts->count())
-                        @foreach($posts as $post)
-                            <article class="col-xs-12 col-sm-6 col-md-3">
-                                <div class="panel panel-info" data-id="{{ $post->id }}">
-                                    <div class="panel-body">
-                                        <a href="https://itsolutionstuff.com/upload/itsolutionstuff.png" title="Nature Portfolio" data-title="Amazing Nature" data-footer="The beauty of nature" data-type="image" data-toggle="lightbox">
-                                            <img src="https://itsolutionstuff.com/upload/itsolutionstuff.png" alt="Nature Portfolio" />
-                                            <span class="overlay"><i class="fa fa-search"></i></span>
-                                        </a>
-                                    </div>  
-                                    <div class="panel-footer">
-                                        <h4><a href="#" title="Nature Portfolio">{{ $post->title }}</a></h4>
-                                        <span class="pull-right">
-                                            <span class="like-btn">
-                                                <i id="like{{$post->id}}" class="glyphicon glyphicon-thumbs-up {{ auth()->user()->hasLiked($post) ? 'like-post' : '' }}"></i> <div id="like{{$post->id}}-bs3">{{ $post->likers()->get()->count() }}</div>
-                                            </span>
-                                        </span>
-                                    </div>
-                                </div>
-                            </article>
-                        @endforeach
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<script type="text/javascript">
-    $(document).ready(function() {     
-
-
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+<script>
+    $(document).ready(function(){ 
+    $('#like-btn').addClass('fa fa-thumbs-up');
+     function loadComment(){
+        var postId = $('#post_id').val();
+        var _token = '{{ csrf_token() }}';
+        $.ajax({
+            type:'POST',
+            url:"{{ route('posts.loadComment') }}",
+            data:{postId:postId,_token:_token},
+            success:function(response) {
+              $('#comment').val('');
+              len = response['comments'].length;
+              var box = '';
+              for(var i=0; i<len; i++){
+                  var id = response['comments'][i]['id'];
+                  var comment = response['comments'][i]['comment'];
+                  var user = response['comments'][i]['user'];
+                  var time = response['comments'][i]['time'];
+                  box += '<div class="commented-section mt-2">';
+                  box += '<div class="d-flex flex-row align-items-center commented-user">';
+                  box += '<h5 class="mr-2">'+user+'</h5><span class="dot mb-1">';
+                  box += '</span><span class="mb-1 ml-2 time" data-time="2019-12-25 00:00:00">'+time+'</span>';
+                  box += '</div><div class="comment-text-sm"><span>'+comment+'</span></div>';
+                  box += '<div class="reply-section">';
+                  box += '<div class="d-flex flex-row align-items-center voting-icons">';
+                  box += '<i class="fa fa-sort-up fa-2x mt-3 hit-voting"></i>';
+                  box += '<i class="fa fa-sort-down fa-2x mb-3 hit-voting"></i>';
+                  box += '<span class="ml-2">15</span><span class="dot ml-2"></span>';
+                  box += '<h6 class="ml-2 mt-1" class="reply-btn">Reply</h6>';
+                  box += '</div></div></div><div class="d-flex flex-row"><input id="reply'+id+'" type="text" class="form-control form-control-sm w-50 reply-text" placeholder="Add Reply">';
+                  box +=  '<button class="btn btn-primary btn-sm ml-2 send-reply-btn" type="button" id="send-reply-btn" data-id="'+id+'">Reply</button>';
+                  box +=  '</div>';
+                  box += '<div class="reply-section mt-2">';
+                 
+                  box += '</div>';
             }
-        });
-
-
-        $('i.glyphicon-thumbs-up, i.glyphicon-thumbs-down').click(function(){    
-            var id = $(this).parents(".panel").data('id');
-            var c = $('#'+this.id+'-bs3').html();
-            var cObjId = this.id;
-            var cObj = $(this);
-
-
-            $.ajax({
-               type:'POST',
-               url:'/ajaxRequest',
-               data:{id:id},
-               success:function(data){
-                  if(jQuery.isEmptyObject(data.success.attached)){
-                    $('#'+cObjId+'-bs3').html(parseInt(c)-1);
-                    $(cObj).removeClass("like-post");
-                  }else{
-                    $('#'+cObjId+'-bs3').html(parseInt(c)+1);
-                    $(cObj).addClass("like-post");
-                  }
-               }
-            });
-
-
-        });      
-
-
-        $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
-            event.preventDefault();
-            $(this).ekkoLightbox();
-        });                                        
-    }); 
-</script>
-@endsection
+              $('#comment-box').html(box);
+              $('#totalComments').text(response.totalComments +' Comments');            
+              $('#totalLikes').text(response.totalLikes +' Likes');            
+             // swal("Thank You!", "You clicked the button!", "success");
+            }
+         });
+     }  
+     loadComment();              
+      $('#comment-btn').click(function(e){
+        e.preventDefault();
+        var postId = $('#post_id').val();
+        var comment = $('#comment').val();
+        var _token = '{{ csrf_token() }}';
+        $.ajax({
+          type:'POST',
+          url:"{{ route('posts.comment') }}",
+          data:{postId:postId,comment:comment,_token:_token},
+          success:function(response) {
+         //   $('#comment').val('');
+            loadComment();
+           // $('#comment-box').html(box);            
+            swal("Thank You!", "You clicked the button!", "success");
+          }
+       });
+      });
+      
+      $('#like-btn').click(function(e){
+        e.preventDefault();
+        var btn = $(this);
+        btn.toggleClass('fa fa-thumbs-up');
+        var postId = $('#post_id').val();
+        var _token = '{{ csrf_token() }}';
+        var actionType = $(this).data('type');
+        $.ajax({
+          type:'POST',
+          url:"{{ route('posts.like') }}",
+          data:{postId:postId,actionType:actionType,_token:_token},
+          success:function(response) {
+            btn.addClass('fa fa-thumbs-down');
+            $('#totalLikes').text(response +' Liked');
+            btn.data('type','dislike');           
+       }
+    });
+});
+$(document).on('click','#send-reply-btn',function(){
+    var i = $(this).data('id');
+    var reply = $('#reply'+i).val();
+    var _token = '{{ csrf_token() }}';
+    $.ajax({
+        type:'POST',
+        url:"{{ route('posts.comment.reply') }}",
+        data:{commentId:i,reply:reply,_token:_token},
+        success:function(response) {
+        $('#reply'+i).val(''); 
+       // swal("Thank You!", "You clicked the button!", "success");      
+     }
+  });
+    
+});
+});
+  </script>
--- resources/views/posts/single.blade.php
+++ resources/views/posts/single.blade.php
@@ -22,36 +22,37 @@
                     <input type="hidden" id="post_id" value="{{ $post->id }}">
                     <input type="text" class="form-control mr-3" id="comment" placeholder="Add comment"><button class="btn btn-primary" type="button" id="comment-btn">Comment</button></div>
                     {{--  Other User Comment  --}}
-            <div id="comment-box">
-            </div>        
-                
-            </div>
+                      <div class="comment-box"></div>
+                      @foreach($post->comments as $comment)
+                    <div class="commented-section mt-2">
+                    <div class="d-flex flex-row align-items-center commented-user">
+                        <h5 class="mr-2">{{ $comment->user->name }}</h5><span class="dot mb-1"></span><span class="mb-1 ml-2">{{ $comment->created_at->diffForHumans() }}</span>
+                    </div>
+                    <div class="comment-text-sm"><span>{{ $comment->comment }}.</span></div>
+                    <div class="reply-section">
+                        <div class="d-flex flex-row align-items-center voting-icons"><i class="fa fa-sort-up fa-2x mt-3 hit-voting"></i><i class="fa fa-sort-down fa-2x mb-3 hit-voting"></i><span class="ml-2">10</span><span class="dot ml-2"></span>
+                              <h6 class="ml-2 mt-1"><a href="#" class="reply-link" class="nav-link" data-id="{{ $comment->id }}"> Reply </a></h6>
+                        </div>
+                         <div class="d-flex flex-row" id="reply-box{{ $comment->id }}">
+                        </div>
+                </div>
+              @endforeach               
         </div>
     </div>
 </div>
+@php $date = date('d-m-y') @endphp
 <script>
-    $(document).ready(function(){ 
-    $('#like-btn').addClass('fa fa-thumbs-up');
-     function loadComment(){
+$(document).ready(function(){ 
+ $('#comment-btn').click(function(e){
+        e.preventDefault();
         var postId = $('#post_id').val();
+        var comment = $('#comment').val();
         var _token = '{{ csrf_token() }}';
-        $.ajax({
-            type:'POST',
-            url:"{{ route('posts.loadComment') }}",
-            data:{postId:postId,_token:_token},
-            success:function(response) {
-              $('#comment').val('');
-              len = response['comments'].length;
-              var box = '';
-              for(var i=0; i<len; i++){
-                  var id = response['comments'][i]['id'];
-                  var comment = response['comments'][i]['comment'];
-                  var user = response['comments'][i]['user'];
-                  var time = response['comments'][i]['time'];
-                  box += '<div class="commented-section mt-2">';
+          var box = '';
+           box += '<div class="commented-section mt-2">';
                   box += '<div class="d-flex flex-row align-items-center commented-user">';
-                  box += '<h5 class="mr-2">'+user+'</h5><span class="dot mb-1">';
-                  box += '</span><span class="mb-1 ml-2 time" data-time="2019-12-25 00:00:00">'+time+'</span>';
+                  box += '<h5 class="mr-2">{{ Auth::user()->name  ? 'You' : ''}}</h5><span class="dot mb-1">';
+                  box += '</span><span class="mb-1 ml-2 time">Just Now</span>';
                   box += '</div><div class="comment-text-sm"><span>'+comment+'</span></div>';
                   box += '<div class="reply-section">';
                   box += '<div class="d-flex flex-row align-items-center voting-icons">';
@@ -59,60 +60,29 @@
                   box += '<i class="fa fa-sort-down fa-2x mb-3 hit-voting"></i>';
                   box += '<span class="ml-2">15</span><span class="dot ml-2"></span>';
                   box += '<h6 class="ml-2 mt-1" class="reply-btn">Reply</h6>';
-                  box += '</div></div></div><div class="d-flex flex-row"><input id="reply'+id+'" type="text" class="form-control form-control-sm w-50 reply-text" placeholder="Add Reply">';
-                  box +=  '<button class="btn btn-primary btn-sm ml-2 send-reply-btn" type="button" id="send-reply-btn" data-id="'+id+'">Reply</button>';
-                  box +=  '</div>';
-                  box += '<div class="reply-section mt-2">';
-                  box += '<h6 class="mr-2 text-danger">'+response.reply[12][0]+'</h6>';
-                  box += '</div>';
-            }
-              $('#comment-box').html(box);
-              $('#totalComments').text(response.totalComments +' Comments');            
-              $('#totalLikes').text(response.totalLikes +' Likes');            
-             // swal("Thank You!", "You clicked the button!", "success");
-            }
-         });
-     }  
-     loadComment();              
-      $('#comment-btn').click(function(e){
-        e.preventDefault();
-        var postId = $('#post_id').val();
-        var comment = $('#comment').val();
-        var _token = '{{ csrf_token() }}';
+                  box += '</div></div></div><div class="d-flex flex-row"><input id="reply" type="text" class="form-control form-control-sm w-50 reply-text" placeholder="Add Reply">';
+                  box +=  '<button class="btn btn-primary btn-sm ml-2 send-reply-btn" type="button" id="send-reply-btn">Reply</button>';
+                  box +=  '</div>';  
         $.ajax({
           type:'POST',
           url:"{{ route('posts.comment') }}",
           data:{postId:postId,comment:comment,_token:_token},
           success:function(response) {
-         //   $('#comment').val('');
-            loadComment();
-           // $('#comment-box').html(box);            
-            swal("Thank You!", "You clicked the button!", "success");
-          }
+          $('#comment').val('');
+          $('.comment-box').prepend(box);
+            
+         }
        });
       });
-      
-      $('#like-btn').click(function(e){
-        e.preventDefault();
-        var btn = $(this);
-        btn.toggleClass('fa fa-thumbs-up');
-        var postId = $('#post_id').val();
-        var _token = '{{ csrf_token() }}';
-        var actionType = $(this).data('type');
-        $.ajax({
-          type:'POST',
-          url:"{{ route('posts.like') }}",
-          data:{postId:postId,actionType:actionType,_token:_token},
-          success:function(response) {
-            btn.addClass('fa fa-thumbs-down');
-            $('#totalLikes').text(response +' Liked');
-            btn.data('type','dislike');           
-       }
-    });
-});
-$(document).on('click','#send-reply-btn',function(){
-    var i = $(this).data('id');
-    var reply = $('#reply'+i).val();
+
+  $(document).on('click','.reply-link',function(e){
+    e.preventDefault();
+    var boxId = $(this).data('id');
+    $('#reply-box'+boxId).html('<input id="reply" type="text" class="form-control form-control-sm w-50 reply-text" placeholder="Add Reply"><button class="btn btn-primary btn-sm ml-2 send-reply-btn" type="button" id="send-reply-btn">Reply</button>');
+  });
+
+  $(document).on('click','#send-reply-btn',function(){
+    var reply = $('#reply').val();
     var _token = '{{ csrf_token() }}';
     $.ajax({
         type:'POST',
@@ -122,9 +92,11 @@ $(document).on('click','#send-reply-btn',function(){
         $('#reply'+i).val(''); 
        // swal("Thank You!", "You clicked the button!", "success");      
      }
-  });
-    
+  }); 
 });
+
 });
-  </script>
+
+
+    </script>
 @endsection
